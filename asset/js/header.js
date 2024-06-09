7f6a3d4ceabf1ecb2c406400de2f6875e432117d
YAO <yamin13y@gmail.com>
2024-06-09 15:51:48 +0200

    [stylisation] style des pages en css et ajout d'élément en html

---
+++ asset/js/header.js
@@ -0,0 +1,27 @@
+// document.getElementById('use-title').addEventListener('click', function () {
+//     document.getElementById('pageUse').style.display = 'none';
+//     document.getElementById('timer-page').style.display = 'block';
+
+// });
+
+// =====================================
+// document.getElementById('switchButton').addEventListener('click', function() {
+//     const headerTitle = document.querySelector('.header_page_2_title');
+//     const backLink = document.querySelector('.header_chrono_page_back');
+//     const backIcon = document.querySelector('.back_icon');
+//     const optionIcon = document.querySelector('.option_icon');
+
+//     if (headerTitle.textContent === 'E-Tickets') {
+//         headerTitle.textContent = 'Votre voyage';
+//         backLink.style.display = 'block';
+//         backIcon.style.display = 'block';
+//         optionIcon.style.display = 'none';
+//         filterImg.style.display = 'none';
+//     } else {
+//         headerTitle.textContent = 'E-Tickets';
+//         backLink.style.display = 'none';
+//         backIcon.style.display = 'none';
+//         optionIcon.style.display = 'block';
+//         filterImg.style.display = 'block';
+//     }
+// });
